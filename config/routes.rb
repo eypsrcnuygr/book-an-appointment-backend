df3e8fb7ed0f8232871ec51825b6d4ebcd700a30
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-01-10 22:04:34 +0300

    Delete comments on route

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   mount_devise_token_auth_for 'Teacher', at: 'auth_teacher'
-  as :teacher do
-    # Define routes for Teacher within this block.
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
