f312281bbd7ce419bfd6d88702058633bb61186d
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-01-11 14:27:33 +0300

    Send the applied students as a json to process in the teachers frontend

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class TeachersController < ApplicationController
   def index
     teacher = Teacher.all
@@ -9,6 +11,15 @@ class TeachersController < ApplicationController
   def show
     current_teacher = Teacher.all.find_by(id: params[:id])
     cur_appointments = current_teacher.appointments
-    render json: cur_appointments
+    cur_appointments_users = []
+
+    cur_appointments.each do |item|
+      cur_appointments_users.push(item.user.email)
+    end
+
+    render json: {
+      appointments: cur_appointments,
+      users_mails: cur_appointments_users
+    }
   end
 end
