3f77694c60c894b57fa6820fe2ca2183d8b39464
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-01-13 01:26:01 +0300

    Fix the explanation fo the teachers controller test

--- spec/controllers/teachers_spec.rb
+++ spec/controllers/teachers_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Teachers', type: :request do
   describe 'GET /teacher' do
     let(:teacher) { Teacher.create(email: 'sercanuygur@gmail.com', password: '12345678', nickname: 'srjnnnn') }
     let(:appointment) { Appointment.create(user_id: 1, teacher_id: teacher.id) }
-    it 'contains the product names' do
+    it 'makes a succesfull request' do
       get "/show/#{teacher.id}"
       expect(response.content_type).to eq 'application/json; charset=utf-8'
       expect(response.status).to be 200
