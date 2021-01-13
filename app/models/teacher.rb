c6fbb2ac4781b35f1548e02f8db0ec4e04ab98e8
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2021-01-13 23:38:42 +0300

    Delete comments on models

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,4 @@
 class Teacher < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   extend Devise::Models
 
   devise :database_authenticatable, :registerable,
